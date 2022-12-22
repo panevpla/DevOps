c28edbdb22d3a1bd63f739de1dabf736f7ca33f3
panevpla <114056833+panevpla@users.noreply.github.com>
2022-12-22 11:14:11 +0200

    iam resources

---
+++ iam_resources.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_role" "prod_ci_role" {
+  name = "prod_ci_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "prod_ci"
+  }
+}
